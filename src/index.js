db5cb7ce0feff39bc88c45c3ac9a42c21cc16e8c
Leslie Ngo <ngo.leslie@gmail.com>
2021-12-17 20:32:25 -0800

    implemented simple CPU opponent, refactored state array to better reflect 3x3 grid

--- src/index.js
+++ src/index.js
@@ -5,20 +5,24 @@ const colors = require('colors');
 
 class TicTacToe {
   constructor() {
-    this.moveState = new Array(9).fill(null);
+    // this.moveState = new Array(9).fill(null);
+    this.moveState = [new Array(3).fill(null), new Array(3).fill(null), new Array(3).fill(null)];
     this.display = '';
     this.currentPlayer = 1;
     this.glyph = 'X'.red;
+    this.playerAmount = null;
+    this.availableCPUMoves = Array.from(Array(9).keys());
+
     this.cellKey = {
-      1: 6,
-      2: 7,
-      3: 8,
-      4: 3,
-      5: 4,
-      6: 5,
-      7: 0,
-      8: 1,
-      9: 2
+      1: [2, 0],
+      2: [2, 1],
+      3: [2, 2],
+      4: [1, 0],
+      5: [1, 1],
+      6: [1, 2],
+      7: [0, 0],
+      8: [0, 1],
+      9: [0, 2],
     }
 
     this.rl = readline.createInterface({
@@ -33,16 +37,16 @@ class TicTacToe {
 
   updateDisplay() {
     this.display =
-    `    ${this.cellManager(7, this.moveState[0])} | ${this.cellManager(8, this.moveState[1])} | ${this.cellManager(9, this.moveState[2])}
+    `    ${this.cellManager(7, this.moveState[0][0])} | ${this.cellManager(8, this.moveState[0][1])} | ${this.cellManager(9, this.moveState[0][2])}
     ---------
-    ${this.cellManager(4, this.moveState[3])} | ${this.cellManager(5, this.moveState[4])} | ${this.cellManager(6, this.moveState[5])}
+    ${this.cellManager(4, this.moveState[1][0])} | ${this.cellManager(5, this.moveState[1][1])} | ${this.cellManager(6, this.moveState[1][2])}
     ---------
-    ${this.cellManager(1, this.moveState[6])} | ${this.cellManager(2, this.moveState[7])} | ${this.cellManager(3, this.moveState[8])}\n`
+    ${this.cellManager(1, this.moveState[2][0])} | ${this.cellManager(2, this.moveState[2][1])} | ${this.cellManager(3, this.moveState[2][2])}\n`
   }
 
   pushDisplay() {
     this.updateDisplay();
-    console.clear();
+    // console.clear();
     console.log(this.display, '\n', `Player ${this.currentPlayer}, please select a cell.`);
   }
 
@@ -51,39 +55,70 @@ class TicTacToe {
       if(![1, 2].some(num => parseInt(input) === num)) {
         console.log('Please enter 1 or 2.');
       } else if(input === '2') {
-        this.start2PlayerGame();
+        this.playerAmount = 2;
+        this.startGame();
       } else {
-        console.log('Sorry, CPU opponent not yet implemented.')
-        this.rl.close();
+        this.playerAmount = 1;
+        this.startGame();
       }
     })
   }
 
-  start2PlayerGame() {
+  startGame() {
     this.pushDisplay();
     this.rl.on('line', input => {
+      const [y, x] = this.cellKey[input];
       if (![1,2,3,4,5,6,7,8,9].some(num => parseInt(input) === num)) {
         this.pushDisplay();
         console.log('Not a valid cell.');
-      } else if (this.moveState[this.cellKey[input]]) {
+      } else if (this.moveState[y][x]) {
         this.pushDisplay();
         console.log('This cell is occupied.')
       } else {
-        this.takeTurn(input);
+        if(this.playerAmount === 2) {
+          this.twoPlayerTurnHandler(input);
+        } else {
+          this.singlerPlayerHandler(input);
+        }
       }
     });
   }
 
-  takeTurn(input) {
-    this.moveState[this.cellKey[input]] = this.glyph;
+  singlerPlayerHandler(input) {
+    const [y, x] = this.cellKey[input];
+    this.moveState[y][x] = this.glyph;
+    this.availableCPUMoves.splice(this.availableCPUMoves.indexOf(parseInt(input) - 1), 1);
+    this.pushDisplay();
     this.checkWin(this.currentPlayer, this.glyph);
-    this.currentPlayer === 1 ? this.currentPlayer = 2 : this.currentPlayer = 1;
-    this.glyph === 'X'.red ? this.glyph = 'O'.yellow : this.glyph = "X".red;
+    this.switchPlayer();
+    this.CPUturnHandler();
+  }
+
+  CPUturnHandler() {
+    const randomMove = this.availableCPUMoves[Math.floor(Math.random() * this.availableCPUMoves.length)];
+    this.availableCPUMoves.splice(this.availableCPUMoves.indexOf(randomMove), 1);
+    const [y, x] = this.cellKey[randomMove + 1];
+    this.moveState[y][x] = this.glyph;
+    this.checkWin(this.currentPlayer, this.glyph);
+    this.switchPlayer();
     this.pushDisplay();
   }
 
+  twoPlayerTurnHandler(input) {
+    const [y, x] = this.cellKey[input];
+    this.moveState[y][x] = this.glyph;
+    this.checkWin(this.currentPlayer, this.glyph);
+    this.switchPlayer();
+    this.pushDisplay();
+  }
+
+  switchPlayer() {
+    this.currentPlayer === 1 ? this.currentPlayer = 2 : this.currentPlayer = 1;
+    this.glyph === 'X'.red ? this.glyph = 'O'.yellow : this.glyph = "X".red;
+  }
+
   checkWin(player, glyph) {
-    const [ a1, a2, a3, b1, b2, b3, c1, c2, c3 ] = this.moveState;
+    const [ [a1, a2, a3], [b1, b2, b3], [c1, c2, c3] ] = this.moveState;
     const row1 = [a1, a2, a3],
           row2 = [b1, b2, b3],
           row3 = [c1, c2, c3],
@@ -100,10 +135,17 @@ class TicTacToe {
       })
     })
 
-    if(win) {
+    if (win) {
       this.updateDisplay()
       this.rl.close();
-      console.log(`${this.display}`, '\n', `Player ${player} wins.`);
+      this.pushDisplay();
+      console.log(`Player ${player} wins.`);
+      process.exit();
+    } else if (this.moveState.every(row => row.every(item => !!item))) {
+      this.updateDisplay()
+      this.rl.close();
+      this.pushDisplay();
+      console.log(`Not a tie. Both are losers.`);
       process.exit();
     }
   }
